39b20ce5509afe135f70467e50ad0b89ab91b314
lccnet105811327 <bigbrain1996@gmail.com>
2024-11-09 10:59:51 +0800

    update

--- Java1/First/src/lesson5/Demo.java
+++ Java1/First/src/lesson5/Demo.java
@@ -1,32 +1,61 @@
 package lesson5;
-//匿名類別，只能被用一次
+//interface只需被用一次時可用匿名類別
 public class Demo {
     public static void main(String[] args) {
 
         //有新增子類別的使用:
-        // 1.匿名類別定義新method可直接使用
+
+        //新增原本不在介面的方法時
+        // 1.直接使用
         new Person(){
             public void fly() {
-                System.out.println("I can fly. --- extends");
+                System.out.println("I can fly.");
             }
         }.fly();
 
-        // 2.匿名類別定義新method無法用其他method呼叫
+        // 2.無法用其他method呼叫
         // personFly(new IRun() {
         //         public void fly() {
         //             System.out.println("I can fly. --- extends");
         //         }
         //     });
 
-        // 直接使用:
-        //用類別呼叫另一個類別，呼叫在main裡面
-        // lambada一行搞定(只能interface才能使用)
-        runMethod1((String name) -> System.out.println(name + " can run fast. -- lambda"));
+
+
+        // 無新增子類別:
+
+        // 1.直接使用(只選一個呼叫)
+        new IRun(){
+            //一定要先覆寫原本的
+            @Override
+            public void run(String name){
+                System.out.println("I can run fast.");
+            }
+            //可新增原本不在介面的方法
+            public void fly(){
+                System.out.println("I can fly.");
+            }
+        }.fly();
+
+        //2.用另一個method呼叫，覆寫在main裡面
+        //一般寫法
+        runMethod1(
+            new IRun() {
+                public void run(String name){
+                    System.out.println(name + " can run fast.");
+                }
+            }
+        );
+        // lambda一行搞定(只能interface才能使用)
+        runMethod2((String name) -> System.out.println(name + " can run fast. -- lambda"));
        
     }
 
-    //創方法在外面，r算是new一個IRun類別的概念
-    public static void runMethod1(IRun r) {
+    //創方法+呼叫在外面，r算是new一個IRun類別的概念
+    public static void runMethod1(IRun r){
+        r.run("Jordan");
+    }
+    public static void runMethod2(IRun r) {
         r.run("David1");
     }
 
